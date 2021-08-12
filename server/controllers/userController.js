d613e927bbdfc7b80543dfda57dd7624838e9a74
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-12 11:16:20 +0530

    user update and details endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -56,3 +56,53 @@ export const userRegister = asyncHandler(async (req, res) => {
     throw new Error('Invalid user data')
   }
 })
+
+// @desc Get user profile
+// @route POST /api/auth/profile
+// @access Private
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+})
+
+// @desc Update user profile
+// @route PUT /api/auth/profile
+// @access Private
+
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateAuthToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+})
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,20 @@
 import express from 'express'
-import { userLogin, userRegister } from '../controllers/userController.js'
+import {
+  userLogin,
+  userRegister,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js'
+
+import { routeProtection } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/register').post(userRegister)
 router.route('/login').post(userLogin)
+router
+  .route('/profile')
+  .get(routeProtection, getUserProfile)
+  .put(routeProtection, updateUserProfile)
 
 export default router
