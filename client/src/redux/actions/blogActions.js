9fea1d91c2033836670a722f62b419b67b77fcec
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-22 20:54:12 +0530

    single blog fetching action

--- client/src/redux/actions/blogActions.js
+++ client/src/redux/actions/blogActions.js
@@ -2,6 +2,9 @@ import {
   FETCH_ALL_BLOGS_REQUEST,
   FETCH_ALL_BLOGS_SUCCESS,
   FETCH_ALL_BLOGS_FAILURE,
+  FETCH_SINGLE_BLOG_REQUEST,
+  FETCH_SINGLE_BLOG_SUCCESS,
+  FETCH_SINGLE_BLOG_FAILURE,
 } from '../constants/blogConstants'
 import axios from 'axios'
 
@@ -16,6 +19,25 @@ export const fetchAllBlogsAction = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: FETCH_ALL_BLOGS_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const fetchSingleBlogAction = (id) => async (dispatch) => {
+  dispatch({ type: FETCH_SINGLE_BLOG_REQUEST })
+
+  try {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_DEV_API}/api/blog/getblog/${id}`
+    )
+    dispatch({ type: FETCH_SINGLE_BLOG_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: FETCH_SINGLE_BLOG_FAILURE,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
